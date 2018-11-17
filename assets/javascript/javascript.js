2f9e41bcee44aea7ec6eeb3df92ce443aad340b4
fallen potato <cruzjon32@gmail.com>
2018-11-17 10:56:32 -0700

    added jquery script tag, added a database, started using brewery api

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,27 @@
+var config = {
+    apiKey: "AIzaSyB9EFbkkslQZKdh_Nm8lGB9KfwZCCHGDWI",
+    authDomain: "brewery-crawl.firebaseapp.com",
+    databaseURL: "https://brewery-crawl.firebaseio.com",
+    projectId: "brewery-crawl",
+    storageBucket: "brewery-crawl.appspot.com",
+    messagingSenderId: "958356463401"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+var queryURL = 
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+}).then(function (response) {
+    
+        
+        
+})
