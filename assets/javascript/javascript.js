524690d0ab4f9d66bca66b8e29dd696ffc3742b6
Lupe <lupec_se@yahoo.com>
2018-11-22 08:54:06 -0700

    Added ajax call and forEach loop to pull from Firebase and iterate through each person in contact list to send SMS.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,6 +9,8 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var ref = database.ref('contacts')
+
 
 
 
@@ -69,12 +71,6 @@ $("#searchBtn").on("click", function () {
 })
 
 // This is the Twilio portion of our javascript
-// Send a SMS when button is clicked!
-
-
-// const message = "Hey " + name + "We are meeting at " + selectedBrewery + " ";
-
-
 
 function clearPersonalInput() {
     $("#nameInput").val("");
@@ -83,27 +79,27 @@ function clearPersonalInput() {
 
 $(document).ready(function () {
 
-    let name = $("#nameInput").val().trim();
-
     $("#submitPersonalInfo").on("click", function (event) {
-
         let name = $("#nameInput").val().trim();
         let number = $("#phoneNumberInput").val().trim();
+        let correctedNumber = number
+            .replace(/[^0-9]/g, '');
 
         var userInfo = {
             name: name,
-            number: number,
+            correctedNumber: correctedNumber,
         }
 
 
-        database.ref().push(userInfo)
+        ref.push(userInfo)
         clearPersonalInput();
 
     });
+    console.log()
 
     // Appending info from Firebase to the table
 
-    database.ref().on("child_added", function(childSnapshot) {
+    database.ref('contacts').on("child_added", function (childSnapshot) {
         let name = childSnapshot.val().name
         $(`
         <tr>
@@ -111,43 +107,40 @@ $(document).ready(function () {
         `).appendTo('#contactList')
     })
 
+    // Send a SMS when button is clicked!
 
-    // Make var that will take the name and have the name 
-    // hold the value of number and pull from Firebase  
     const message = "Hey meet us at "
- 
 
     $("#submitSendSMS").click(function () {
 
         const SID = "ACde7d929d4b9b0f7e32b6f0f553fe9667"
         const Key = "41cdc646ad2521c5e86216b3b17dca1b"
-        
-        database.ref.once('value', function (snapshot) {
+        database.ref('contacts').once('value', function (snapshot) {
             snapshot.forEach(function (childSnapshot) {
                 var childKey = childSnapshot.key;
                 var childData = childSnapshot.val();
-                // ...
+                let name = childSnapshot.val().correctedNumber;
+                console.log(name);
+
+                $.ajax({
+                    type: 'POST',
+                    url: 'https://api.twilio.com/2010-04-01/Accounts/' + SID + '/Messages.json',
+                    data: {
+                        "To": "+1" + name,
+                        "From": "+19562671699",
+                        "Body": message,
+                    },
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "Basic " + btoa(SID + ':' + Key));
+                    },
+                    success: function (data) {
+                        console.log(data);
+                    },
+                    error: function (data) {
+                        console.log(data);
+                    }
+                });
             });
         });
-
-
-        // $.ajax({
-        //     type: 'POST',
-        //     url: 'https://api.twilio.com/2010-04-01/Accounts/' + SID + '/Messages.json',
-        //     data: {
-        //         "To": name,
-        //         "From": "+19562671699",
-        //         "Body": message,
-        //     },
-        //     beforeSend: function (xhr) {
-        //         xhr.setRequestHeader("Authorization", "Basic " + btoa(SID + ':' + Key));
-        //     },
-        //     success: function (data) {
-        //         console.log(data);
-        //     },
-        //     error: function (data) {
-        //         console.log(data);
-        //     }
-        // });
     });
 });
