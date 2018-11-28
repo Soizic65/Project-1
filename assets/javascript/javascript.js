aebb2b87ad41688ae6abe269b638ab76c0d1ad66
fallen potato <cruzjon32@gmail.com>
2018-11-27 20:20:02 -0700

    changed http to https again

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -107,6 +107,12 @@ $("#searchBtn").on("click", function () {
 
 })
 
+$("#theForm").submit(function() {
+    $("#searchBtn").click()
+    return false
+})
+
+
 // This is the Twilio portion of our javascript
 
 function clearPersonalInput() {
